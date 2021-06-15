10d4deae23567a5e805d48e334861a9c03f4f354
takefy-dev <jules001dixon@gmail.com>
2021-06-16 00:39:50 +0200

    finish api routes deezer & spotify & optimize searching playlist

--- routes/spotify/index.js
+++ routes/spotify/index.js
@@ -1,23 +1,31 @@
 const router = require('express').Router();
-const { spotifyArtistOccurence, firstXName } = require('../../utils/utils');
+const { spotifyArtistOccurence, firstXName,spotifyCheckPlaylist } = require('../../utils/utils');
 
-router.post('/id/:id', async (req, res) => {
+router.post('/:id', async (req, res) => {
     const { id } = req.params;
     const manager = req.app.get('manager');
     const playlist = await manager.spotifyApi.playlists.getTracks(id);
     if(playlist.total === 0) return res.status(404).json({error: 'Playlist not found'})
     const occurence = spotifyArtistOccurence(playlist.items).filter(artist => artist.occurence > 1).sort((a,b) => b.occurence - a.occurence)
 
-    let x = 5
-    let names = firstXName(x, occurence)
-    let preferedPlaylist = await manager.spotifyApi.playlists.search(names, {limit: 20, type: ['playlist']})
+    let x = 6
+    let names = firstXName(x, occurence).split('-').join(' ')
+    let preferedPlaylist = await manager.spotifyApi.playlists.search(names, {limit: 20, type: ['playlist']});
     while(preferedPlaylist.total === 0 && x > 0){
         x-=1
-        names = firstXName(x, occurence)
-        console.log(names)
+        names = firstXName(x, occurence).split('-').join(' ')
         preferedPlaylist  = await manager.spotifyApi.playlists.search(names, {limit: 20, type: ['playlist']})
     }
-    return res.status(200).json({tracks: playlist.items, occurence: Object.fromEntries(occurence), preferedPlaylist: preferedPlaylist.items})
+    let finalPreferedPlaylist = []
+    for(const playlist of preferedPlaylist.items){
+        const playlistChecked = await spotifyCheckPlaylist(playlist.id, manager.spotifyApi, names);
+        if(playlistChecked){
+            playlist.url = playlist.externalUrls.spotify;
+            console.log(playlist.url)
+            finalPreferedPlaylist.push(playlist);
+        }
+    }
+    return res.status(200).json({tracks: playlist.items, occurence: Object.fromEntries(occurence), preferedPlaylist: finalPreferedPlaylist})
 })
 
 
--- utils/utils.js
+++ utils/utils.js
@@ -2,8 +2,6 @@ const Collection = require('./collection')
 
 function spotifyArtistOccurence(playlistTracks) {
     const result = new Collection();
-
-
     for(const tracks of playlistTracks){
         const track = tracks.track
         const artists= track.artists;
@@ -15,19 +13,61 @@ function spotifyArtistOccurence(playlistTracks) {
                 result.set(artist.name, {id: artist.id, occurence: 1, name: artist.name});
             }
         }
-
     }
     return result
+}
+
+function deezerArtistOccurence(playlistTracks) {
+    const result = new Collection();
+    for(const track of playlistTracks.data){
+            const { artist } = track
+            if(result.has(artist.name)){
+                let occ = result.get(artist.name).occurence;
+                result.set(artist.name, {id: artist.id, occurence: occ+1, name: artist.name})
+            }else {
+                result.set(artist.name, {id: artist.id, occurence: 1, name: artist.name});
+            }
+    }
+    return result;
 }
 
 function firstXName(x, occurence){
     let i = 0;
     const names=  occurence.map((artist) => {
         i++;
-        return i < x ? artist.name : ''
+        return i < x ? artist.name.split(' ').join('-') : ''
 
     })
     return names.join(' ');
 }
 
-module.exports = { spotifyArtistOccurence,firstXName };
+async function spotifyCheckPlaylist(playlistID, spotifyClient, artistQuery){
+    const playlistTracks = await spotifyClient.playlists.getTracks(playlistID);
+    let i = 0
+
+    for(const tracks of playlistTracks.items){
+        const track = tracks.track;
+        const artists = track.artists;
+        let names = artistQuery.split(' ').filter(x => x !== '');
+        artists.forEach(artist => {
+            if(names.includes(artist.name.split(' ').join('-'))) i++;
+        })
+    }
+    if(playlistTracks.items.length / 2 > i) return playlistID;
+    return undefined;
+}
+
+async function deezerCheckPlaylist(id, deezerManager, artistQuery){
+    let i = 0;
+    const playlist = await deezerManager.playlist(id);
+    const tracks = playlist.tracks.data;
+    for(const track of tracks){
+        const { artist } = track;
+        let names = artistQuery.split(' ').filter(x => x !== '');
+        if(names.includes(artist.name.split(' ').join('-'))) i++;
+    }
+    if(tracks.length / 2 > i) return playlist.id;
+    return undefined;
+}
+
+module.exports = { spotifyArtistOccurence,firstXName, spotifyCheckPlaylist, deezerCheckPlaylist, deezerArtistOccurence };
